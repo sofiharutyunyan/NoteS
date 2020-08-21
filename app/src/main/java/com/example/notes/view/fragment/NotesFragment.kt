6f4c97189d19d5fd730a2e7eb7894369cfddbdb8
Sofi <sofi.harutyunyan95@gmail.com>
2020-08-21 15:39:17 +0400

    Added ViewModel class for Notes fragment: still empty

--- app/src/main/java/com/example/notes/view/fragment/NotesFragment.kt
+++ app/src/main/java/com/example/notes/view/fragment/NotesFragment.kt
@@ -8,24 +8,27 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.notes.R
+import com.example.notes.databinding.FragmentNotesBinding
+import com.example.notes.view.fragment.base.BaseFragmentWithViewModel
+import com.example.notes.viewmodel.base.NotesViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class NotesFragment : Fragment() {
-
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes, container, false)
-    }
+class NotesFragment : BaseFragmentWithViewModel<FragmentNotesBinding, NotesViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
             //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
 
+    }
+
+    override fun getViewModelClass(): Class<NotesViewModel> {
+        return NotesViewModel::class.java
+    }
+
+    override fun getLayoutResource(): Int {
+        return R.layout.fragment_notes
     }
 }
---
+++ app/src/main/java/com/example/notes/viewmodel/base/NotesViewModel.kt
@@ -0,0 +1,3 @@
+package com.example.notes.viewmodel.base
+
+class NotesViewModel : BaseViewModel()
